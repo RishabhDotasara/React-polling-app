8dbdc0fed9d460d27bb66cb67876f64ea9e5fc96
Rishabh Dotasara <rishabhdotasara@gmail.com>
2024-05-29 21:36:19 +0530

    changes

--- pollapp/src/App.jsx
+++ pollapp/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import Home from './components/Home'
 import {BrowserRouter, Routes, Route, useParams, Link} from "react-router-dom"
@@ -10,9 +9,7 @@ import Poll from './components/Poll'
 import Result from './components/Result'
 
 function App() {
-  const [count, setCount] = useState(0);
-  const {id} = useParams();
-
+ 
   return (
     <BrowserRouter>
       {/* <h1>This is a page.</h1> */}
